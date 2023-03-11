7fb43f43474969583e495e77896b901c2a03f6b1
Samantha <samanthax0326@gmail.com>
2023-03-11 15:51:42 -0800

    create labels and entries

--- PWManager/main.py
+++ PWManager/main.py
@@ -13,8 +13,28 @@ window.config(padx=20, pady=20)
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img) #tuple of x coor., y coor., and img
-canvas.pack()
+canvas.grid(row=0, column=1)
 
+#labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+pw_label = Label(text="Password:")
+pw_label.grid(row=3, column=0)
+
+#entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1)
+pw_entry = Entry(width=21)
+pw_entry.grid(row=3, column=1)
+
+#buttons
+generate_pw_button = Button(text="Generate Password")
+
+ad_button = Radiobutton(text="Add")
 
 
 window.mainloop()
